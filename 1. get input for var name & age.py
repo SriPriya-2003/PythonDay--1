2beb8a17947a2e6865e63c48a67968769761986e
SriPriya-2003 <131514547+SriPriya-2003@users.noreply.github.com>
2023-09-23 13:09:36 -0400

    Add files via upload

---
+++ 1. get input for var name & age.py
@@ -0,0 +1,4 @@
+a=  str("sripriya")
+b=int(19)
+print("my name is:",a)
+print("my age is:",b)
---
+++ 2. Get ip for var name,age,address.py
@@ -0,0 +1,6 @@
+Name=input()
+Age=input()
+Address=input()
+print("My name is:",Name)
+print("My Age is:",Age)
+print("My address is:",Address)
